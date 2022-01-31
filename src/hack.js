8f483ab204f7f28cf78e63e9fba1e9ee4ecacc22
rubenpve <rverdugo@travix.com>
2022-01-31 21:34:47 +0100

    Add catches to avoid inifnit loops when cannot hack something

--- src/hack.js
+++ src/hack.js
@@ -6,25 +6,36 @@ const SCRIPT_NAME = 'hack.js'
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    const currentServer = ns.getHostname()
-    while (true) {
-        const reachableServers = findServers(ns, currentServer)
-        const availableRam = ns.getServerMaxRam(currentServer) - ns.getServerUsedRam(currentServer)
-        const scriptRam = ns.getScriptRam(SCRIPT_NAME)
-        const maxScriptsInMemory = Math.floor(availableRam / scriptRam)
-        ns.tprint(`Available RAM: ${availableRam}\nScript RAM: ${scriptRam}\nMax number of runs with available RAM: ${maxScriptsInMemory}`)
-        const threadsPerTarget = Math.max(1, Math.floor(maxScriptsInMemory / reachableServers.length))
-        const remainingThreads = Math.max(0, Math.floor(maxScriptsInMemory - threadsPerTarget * reachableServers.length))
-        ns.tprint(`Reachable servers: ${reachableServers}\nThreads per server: ${threadsPerTarget}\nRemaining threads: ${remainingThreads}`)
-        for (const [i, targetServer] of reachableServers.entries()) {
-            const execThreads = i === 0 ? threadsPerTarget + remainingThreads : threadsPerTarget
-            // const pid = ns.exec('root.js', targetServer, 1)
-            if(!ns.isRunning(SCRIPT_NAME, targetServer, 1)){
-                ns.exec(SCRIPT_NAME, targetServer, 1)
+    try {
+        const currentServer = ns.getHostname()
+        while (true) {
+            const reachableServers = findServers(ns, currentServer)
+            const availableRam = ns.getServerMaxRam(currentServer) - ns.getServerUsedRam(currentServer)
+            const scriptRam = ns.getScriptRam(SCRIPT_NAME)
+            const maxScriptsInMemory = Math.floor(availableRam / scriptRam)
+            ns.tprint(`Available RAM: ${availableRam}\nScript RAM: ${scriptRam}\nMax number of runs with available RAM: ${maxScriptsInMemory}`)
+            const threadsPerTarget = Math.max(1, Math.floor(maxScriptsInMemory / reachableServers.length))
+            const remainingThreads = Math.max(0, Math.floor(maxScriptsInMemory - threadsPerTarget * reachableServers.length))
+            ns.tprint(`Reachable servers: ${reachableServers}\nThreads per server: ${threadsPerTarget}\nRemaining threads: ${remainingThreads}`)
+            for (const [i, targetServer] of reachableServers.entries()) {
+                const execThreads = i === 0 ? threadsPerTarget + remainingThreads : threadsPerTarget
+                // const pid = ns.exec('root.js', targetServer, 1)
+                if (!ns.isRunning(SCRIPT_NAME, targetServer, 1)) {
+                    ns.exec(SCRIPT_NAME, targetServer, 1)
+                }
+                try{
+                    ns.exec('do_hack.js', currentServer, execThreads, targetServer, execThreads)
+                } catch (e){
+                    ns.tprint(`Couldn't hack: ${e.toString()}`)
+                    await ns.sleep(1*60*1000)
+                }
             }
-            ns.exec('do_hack.js', currentServer, execThreads, targetServer, execThreads)
+            await ns.sleep(sleepTime)
         }
-        await ns.sleep(sleepTime)
+    } catch (e){
+        ns.tprint(`Couldn't hack: ${e.toString()}`)
+        await ns.sleep(1*60*1000)
+
     }
 }
 
