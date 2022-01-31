ee20167679fa0b27659cfbee0f13b524f2fe4e7d
rubenpve <rverdugo@travix.com>
2022-01-31 21:15:18 +0100

    Fix issues

--- src/spread.js
+++ src/spread.js
@@ -7,33 +7,32 @@ export const FLAG_FILE = '.29.txt'
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    try {
-        const currentServer = ns.getHostname()
-        const id = ns.args[0] ? ns.args[0] : new Date().getTime().toString()
-        if (id === ns.read(FLAG_FILE)) {
-            ns.tprint(`Skipping ${currentServer}: already infected`)
-            return
+    const currentServer = ns.getHostname()
+    const id = ns.args[0] ? ns.args[0] : new Date().getTime().toString()
+    //Update files in home
+    if (!ns.args[0]) {
+        await installFiles(ns, currentServer)
+    }
+    if (id === ns.read(FLAG_FILE)) {
+        ns.tprint(`Skipping ${currentServer}: already infected`)
+        return
+    }
+    ns.tprint(`Spreading from ${currentServer}`)
+    await ns.write(FLAG_FILE, id, "w")
+    const reachableServers = ns.scan()
+    for (let server of reachableServers) {
+        try {
+            await installFiles(ns, server)
+            ns.exec(THIS_NAME, server, 1, id)
+            await ns.sleep(sleepSeconds * 1000)
+        } catch (e) {
+            ns.tprint(`Error spreading to ${server}: ${e.toString()}`)
         }
-        ns.tprint(`Spreading from ${currentServer}`)
-        await installFiles(ns)
-        await ns.write(FLAG_FILE, id, "w")
-        const reachableServers = ns.scan()
-        for (let server of reachableServers) {
-            try {
-                ns.exec(THIS_NAME, server, 1, id)
-                await ns.sleep(sleepSeconds * 1000)
-            } catch (e) {
-                ns.tprint(`Error spreading to ${server}: ${e.toString()}`)
-            }
-        }
-    } catch (e) {
-        ns.print(`An error occurred while spreading: ${e.toString()}`)
-        await ns.sleep(10*60*1000)
     }
 }
 
-export async function installFiles(ns) {
+export async function installFiles(ns, server) {
     for (const file of FILES) {
-        await ns.wget(FILES_ROOT + file, file)
+        await ns.wget(FILES_ROOT + file, file, server)
     }
 }
