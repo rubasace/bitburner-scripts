1a8b8bf3126435c3d00346a7359db2f6a08bb6f7
rubenpve <rverdugo@travix.com>
2022-01-31 19:04:08 +0100

    Add final sleep to be able to check logs

--- src/spread.js
+++ src/spread.js
@@ -4,27 +4,34 @@ const sleepSeconds = 1
 
 
 export const FILES = [THIS_NAME, 'root.js', 'hack.js', 'cleanup.js']
+// export const OWN_SERVERS = ['home', 'nasvigo', 'darkweb']
 export const FLAG_FILE = '.29.txt'
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    const currentServer = ns.getHostname()
-    const id = ns.args[0] ? ns.args[0] : new Date().getTime().toString()
-    if (id === ns.read(FLAG_FILE)) {
-        ns.tprint(`Skipping ${currentServer}: already infected`)
-        return
-    }
-    ns.tprint(`Spreading from ${currentServer}`)
-    await ns.write(FLAG_FILE, id, "w")
-    const reachableServers = ns.scan()
-    for (let server of reachableServers) {
-        try {
-            await installFiles(ns, server)
-            ns.exec(THIS_NAME, server, id)
-            await ns.sleep(sleepSeconds * 1000)
-        } catch (e) {
-            ns.tprint(`Error spreading to ${server}: ${e.toString()}`)
+    try {
+        const currentServer = ns.getHostname()
+        const id = ns.args[0] ? ns.args[0] : new Date().getTime().toString()
+        if (id === ns.read(FLAG_FILE)) {
+            ns.tprint(`Skipping ${currentServer}: already infected`)
+            return
         }
+        ns.tprint(`Spreading from ${currentServer}`)
+        await ns.write(FLAG_FILE, id, "w")
+        const reachableServers = ns.scan()
+        for (let server of reachableServers) {
+            try {
+                await installFiles(ns, server)
+                ns.exec(THIS_NAME, server, id)
+                await ns.sleep(sleepSeconds * 1000)
+            } catch (e) {
+                ns.tprint(`Error spreading to ${server}: ${e.toString()}`)
+            }
+        }
+    } catch (e) {
+        ns.print(`An error ocurred while spreading: ${e.toString()}`)
+    } finally {
+        await ns.sleep(10 * 60 * 1000)
     }
 }
 
