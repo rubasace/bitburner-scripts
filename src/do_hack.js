ad1588d44577f476ac9c22ae9cd506bc4f9ddbbd
rubenpve <rverdugo@travix.com>
2022-01-31 21:37:10 +0100

    Fix catches

--- src/do_hack.js
+++ src/do_hack.js
@@ -4,20 +4,26 @@ const TARGET_MONEY = 0.75
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    const target = ns.args[0]
-    const threads = ns.args[1] ? ns.args[1] : 1
+    try {
+        const target = ns.args[0]
+        const threads = ns.args[1] ? ns.args[1] : 1
 
-    const maxMoney = await ns.getServerMaxMoney(target)
-    let minSecurityLevel = await ns.getServerMinSecurityLevel(target)
-    const currentMoney = await ns.getServerMoneyAvailable(target)
-    const currentSecurityLevel = await ns.getServerSecurityLevel(target)
-    if (currentSecurityLevel > minSecurityLevel + TARGET_SECURITY) {
-        await ns.weaken(target, {threads})
-    } else if (currentMoney < maxMoney * TARGET_MONEY) {
-        await ns.grow(target, {threads})
-    } else {
-        await ns.hack(target, {threads})
+        const maxMoney = await ns.getServerMaxMoney(target)
+        let minSecurityLevel = await ns.getServerMinSecurityLevel(target)
+        const currentMoney = await ns.getServerMoneyAvailable(target)
+        const currentSecurityLevel = await ns.getServerSecurityLevel(target)
+        if (currentSecurityLevel > minSecurityLevel + TARGET_SECURITY) {
+            await ns.weaken(target, {threads})
+        } else if (currentMoney < maxMoney * TARGET_MONEY) {
+            await ns.grow(target, {threads})
+        } else {
+            await ns.hack(target, {threads})
+        }
+        await ns.sleep(sleepTime)
+    } catch (e) {
+        ns.tprint(`Couldn't hack: ${e.toString()}`)
+        await ns.sleep(1 * 60 * 1000)
+
     }
-    await ns.sleep(sleepTime)
 
 }
