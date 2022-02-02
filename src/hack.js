855a55df0dc0692e0ebd788437a97da48fd51abb
rubenpve <rverdugo@travix.com>
2022-02-02 16:17:48 +0100

    Minor fix

--- src/hack.js
+++ src/hack.js
@@ -34,7 +34,7 @@ export async function main(ns) {
 
 async function executeAndWait(ns, script, server, ...args) {
     const pid = ns.exec(script, server, 1, ...args)
-    while (ns.isRunning(pid)) {
+    while (pid !== 0 && ns.isRunning(pid)) {
         await ns.sleep(sleepTime)
     }
 }
--- src/spread.js
+++ src/spread.js
@@ -29,7 +29,7 @@ export async function main(ns) {
 //TODO move to common utility
 async function executeAndWait(ns, script, server, ...args) {
     const pid = ns.exec(script, server, 1, ...args)
-    while (ns.isRunning(pid)) {
+    while (pid !== 0 && ns.isRunning(pid)) {
         await ns.sleep(sleepTime)
     }
 }
