d2e294450e8d6b8d4646475ebbf42818b91058d3
rubenpve <rverdugo@travix.com>
2022-02-02 13:32:58 +0100

    Fix varargs syntax

--- src/hack.js
+++ src/hack.js
@@ -30,7 +30,7 @@ export async function main(ns) {
 }
 
 async function executeAndWait(ns, script, server, ...args) {
-    const pid = ns.exec(script, server, 1, args)
+    const pid = ns.exec(script, server, 1, ...args)
     while (ns.isRunning(pid)) {
         await ns.sleep(sleepTime)
     }
