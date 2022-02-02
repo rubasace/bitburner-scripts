aa4206e9bf2f02de0190587055089e07afce38e4
rubenpve <rverdugo@travix.com>
2022-02-02 13:50:33 +0100

    Fix install

--- src/install.js
+++ src/install.js
@@ -5,7 +5,7 @@ export const FILES = [THIS_NAME, 'spread.js', 'root.js', 'hack.js', 'cleanup.js'
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    ns.tprint(`installing files in ${ns.getHostName()}`)
+    ns.tprint(`installing files in ${ns.getHostname()}`)
     for (const file of FILES) {
         await ns.wget(FILES_ROOT + file, file, server)
     }
